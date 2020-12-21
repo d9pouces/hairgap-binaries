9494a1a1fd6fa54226c9d0101ecf939543d8b8fa
d9pouces <github@19pouces.net>
2020-12-21 21:17:45 +0100

    New binary checks

--- README.md
+++ README.md
@@ -13,15 +13,15 @@ vagrant plugin install vagrant-scp
 
 # compile binaries in a Vagrant box 
 vagrant up centos_8
-vagrant scp centos_8:/tmp/hairgap/hairgaps hairgap_binaries/manylinux1_x86_64-hairgaps
-vagrant scp centos_8:/tmp/hairgap/hairgapr hairgap_binaries/manylinux1_x86_64-hairgapr
+vagrant scp centos_8:/tmp/hairgap/hairgaps hairgap_binaries/manylinux2014_x86_64-hairgaps
+vagrant scp centos_8:/tmp/hairgap/hairgapr hairgap_binaries/manylinux2014_x86_64-hairgapr
 vagrant destroy centos_8 --force
 
 # check these binaries on a Debian 9 / CentOS 7 / Xenial / Bionic / Focal 
 for k in centos_7 debian_9 ubuntu_xenial ubuntu_bionic ubuntu_focal; do
     vagrant up $k > /dev/null 2> /dev/null && \
-    vagrant scp hairgap_binaries/manylinux1_x86_64-hairgaps $k:/tmp/hairgaps > /dev/null 2> /dev/null && \
-    vagrant scp hairgap_binaries/manylinux1_x86_64-hairgapr $k:/tmp/hairgapr > /dev/null 2> /dev/null && \
+    vagrant scp hairgap_binaries/manylinux2014_x86_64-hairgaps $k:/tmp/hairgaps > /dev/null 2> /dev/null && \
+    vagrant scp hairgap_binaries/manylinux2014_x86_64-hairgapr $k:/tmp/hairgapr > /dev/null 2> /dev/null && \
     vagrant ssh $k -c "/tmp/hairgaps -h" 2> /dev/null | grep sender > /dev/null && echo "$k : hairgaps valid" && \
     vagrant ssh $k -c "/tmp/hairgapr -h" 2> /dev/null | grep receiver > /dev/null && echo "$k : hairgapr valid" && \
     vagrant destroy --force $k > /dev/null 2> /dev/null
--- hairgap_binaries/test_hairgap_binaries.py
+++ hairgap_binaries/test_hairgap_binaries.py
@@ -1,20 +1,32 @@
+import os
 from unittest import TestCase, mock
 
 import pkg_resources
 
-from hairgap_binaries import get_hairgapr
+from hairgap_binaries import get_hairgapr, get_hairgaps
 
 
 class TestOnLinux(TestCase):
     # noinspection PyUnusedLocal
     @mock.patch("pkg_resources.get_platform", side_effect=lambda: "linux-x86_64")
-    def test_on_linux(self, mock_):
+    def test_on_linux_hairgapr(self, mock_):
+        target = pkg_resources.resource_filename("hairgap_binaries", "manylinux2014_x86_64-hairgapr")
         self.assertEqual(
-            pkg_resources.resource_filename(
-                "hairgap_binaries", "manylinux2014_x86_64-hairgapr"
-            ),
+            target,
             get_hairgapr(),
         )
+        self.assertTrue(os.path.isfile(target))
+
+    # noinspection PyUnusedLocal
+    @mock.patch("pkg_resources.get_platform", side_effect=lambda: "linux-x86_64")
+    def test_on_linux_hairgaps(self, mock_):
+        target = pkg_resources.resource_filename("hairgap_binaries",
+                                                 "manylinux2014_x86_64-hairgaps")
+        self.assertEqual(
+            target,
+            get_hairgaps(),
+        )
+        self.assertTrue(os.path.isfile(target))
 
 
 class TestOnMacosX(TestCase):
@@ -22,5 +34,11 @@ class TestOnMacosX(TestCase):
     @mock.patch(
         "pkg_resources.get_platform", side_effect=lambda: "macosx-10.14.6-x86_64"
     )
-    def test_on_linux(self, mock_):
+    def test_on_macos_hairgapr(self, mock_):
         self.assertIsNone(get_hairgapr())
+    # noinspection PyUnusedLocal
+    @mock.patch(
+        "pkg_resources.get_platform", side_effect=lambda: "macosx-10.14.6-x86_64"
+    )
+    def test_on_macos_hairgaps(self, mock_):
+        self.assertIsNone(get_hairgaps())
