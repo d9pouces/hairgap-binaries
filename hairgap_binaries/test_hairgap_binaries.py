f2ca6e31d60328c9fa52304d875b2c885d4539fa
d9pouces <github@19pouces.net>
2023-11-06 23:16:12 +0100

    ci: update pre-commit

--- hairgap_binaries/test_hairgap_binaries.py
+++ hairgap_binaries/test_hairgap_binaries.py
@@ -1,18 +1,18 @@
-# ##############################################################################
-#  This file is part of Hairgap                                                #
-#                                                                              #
-#  Copyright (C) 2020 Matthieu Gallet <github@19pouces.net>                    #
-#  All Rights Reserved                                                         #
-#                                                                              #
-#  You may use, distribute and modify this code under the                      #
-#  terms of the (BSD-like) CeCILL-B license.                                   #
-#                                                                              #
-#  You should have received a copy of the CeCILL-B license with                #
-#  this file. If not, please visit:                                            #
-#  https://cecill.info/licences/Licence_CeCILL-B_V1-en.txt (English)           #
-#  or https://cecill.info/licences/Licence_CeCILL-B_V1-fr.txt (French)         #
-#                                                                              #
-# ##############################################################################
+# ############################################################################
+#  This file is part of Hairgap                                              #
+#                                                                            #
+#  Copyright (C) 2020 Matthieu Gallet <github@19pouces.net>                  #
+#  All Rights Reserved                                                       #
+#                                                                            #
+#  You may use, distribute and modify this code under the                    #
+#  terms of the (BSD-like) CeCILL-B license.                                 #
+#                                                                            #
+#  You should have received a copy of the CeCILL-B license with              #
+#  this file. If not, please visit:                                          #
+#  https://cecill.info/licences/Licence_CeCILL-B_V1-en.txt (English)         #
+#  or https://cecill.info/licences/Licence_CeCILL-B_V1-fr.txt (French)       #
+#                                                                            #
+# ############################################################################
 import os
 from unittest import TestCase, mock
 
@@ -33,17 +33,17 @@ class TestOnLinux(TestCase):
         self.assertTrue(os.path.isfile(target))
 
 
+macos = "macosx-10.14.6-x86_64"
+
+
 class TestOnMacosX(TestCase):
     # noinspection PyUnusedLocal
-    @mock.patch(
-        "sysconfig.get_platform", side_effect=lambda: "macosx-10.14.6-x86_64"
-    )
+
+    @mock.patch("sysconfig.get_platform", side_effect=lambda: macos)
     def test_on_macos_hairgapr(self, mock_):
         self.assertIsNone(get_hairgapr())
 
     # noinspection PyUnusedLocal
-    @mock.patch(
-        "sysconfig.get_platform", side_effect=lambda: "macosx-10.14.6-x86_64"
-    )
+    @mock.patch("sysconfig.get_platform", side_effect=lambda: macos)
     def test_on_macos_hairgaps(self, mock_):
         self.assertIsNone(get_hairgaps())
